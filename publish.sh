12171b755119fa0f577740271dfc669867850f27
Sarantopoulos Konstantinos <sarantopouloskon@gmail.com>
2022-08-26 15:24:29 +0300

    Fix publish script

--- publish.sh
+++ publish.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 export PACKAGE_FOLDER=$1
-echo publishing $GIT_TAG_VERSION
+export GIT_TAG_VERSION=$(git describe --tags --abbrev=0)
+export TMP_PACKAGE_JSON=/tmp/package_$PACKAGE_FOLDER.json
+echo $TMP_PACKAGE_JSON
+echo Publishing $PACKAGE_FOLDER@$GIT_TAG_VERSION
 cd packages/$PACKAGE_FOLDER
+# Update package.json's version with the tag from the release
+jq ".version=\"$GIT_TAG_VERSION\"" package.json > $TMP_PACKAGE_JSON && mv $TMP_PACKAGE_JSON package.json
 if [[ $GIT_TAG_VERSION == *"beta"* ]];
 then
 echo "Publishing beta version $GIT_TAG_VERSION";
