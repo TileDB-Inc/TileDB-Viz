e6ac69ddb2a3845852dd3615b702b8994bb8d9e7
Margriet Groenendijk <margriet.groenendijk@gmail.com>
2023-01-06 14:26:10 +0000

    Update Image.stories.tsx

--- packages/react/src/stories/Image.stories.tsx
+++ packages/react/src/stories/Image.stories.tsx
@@ -15,13 +15,56 @@ export default {
 
 const Template = () => (
   <ImageVisualization
-    xyBbox={[11500, 14000, 11500, 14000]}
     data={uintArray.buffer}
     zScale={5}
     moveSpeed={-1}
     wheelPrecision={-1}
     inspector={false}
+    chartType="bar"
+    //bbox={{
+    //  X: [9500, 14000],
+    //  Y: [9500, 14000]
+    //}}
   />
 );
 
 export const Image = Template.bind({});
+
+const token = process.env.STORYBOOK_REST_TOKEN;
+const namespace = '***';
+const arrayName = '***';
+
+// maxLevel=4, height=100, arrayName='' for cube test
+
+export const ImageInteractive = () => (
+  <ImageVisualization
+    data={uintArray.buffer}
+    arrayName={arrayName}
+    namespace={namespace}
+    token={token}
+    //band={1}
+    //bbox={{
+    //  X: [9500, 14000],
+    //  Y: [9500, 14000]
+    //}}
+    zScale={5}
+    moveSpeed={-1}
+    wheelPrecision={-1}
+    inspector={false}
+    chartType="interactive"
+  />
+);
+
+export const ImageMapbox = () => (
+  <ImageVisualization
+    data={uintArray.buffer}
+    arrayName={arrayName}
+    namespace={namespace}
+    token={token}
+    zScale={5}
+    moveSpeed={-1}
+    wheelPrecision={-1}
+    inspector={false}
+    chartType="mapbox"
+  />
+);
