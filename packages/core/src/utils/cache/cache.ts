d9f09a359abcd36478e66cf47a73b3b71e5baa14
Sarantopoulos Konstantinos <sarantopouloskon@gmail.com>
2023-04-12 14:33:16 +0300

    Invalidate key ranges that don't match specific bounds

--- packages/core/src/utils/cache/cache.ts
+++ packages/core/src/utils/cache/cache.ts
@@ -31,10 +31,22 @@ const getCacheDB = async (storeName: string) => {
 
 export const getQueryDataFromCache = async (storeName: string, key: number) => {
   const db = await getCacheDB(storeName);
+
   const value = await db.get(storeName, key);
   return value;
 };
 
+export const invalidateKeys = async (storeName: string, bound: number) => {
+  const db = await getCacheDB(storeName);
+  const tx = db.transaction(storeName, 'readwrite');
+  const index = tx.store.index(INDEX_NAME);
+  for await (const cursor of index.iterate(
+    IDBKeyRange.upperBound(bound, true)
+  )) {
+    await cursor.delete();
+  }
+};
+// invalidateKeys('TileDB-Inc:autzen-classified', 1681293670742);
 export const writeToCache = async (
   storeName: string,
   key: number,
