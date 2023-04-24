44903efce45c26fade56581588ae66e7e19fd425
Sarantopoulos Konstantinos <sarantopouloskon@gmail.com>
2023-04-24 15:21:45 +0300

    Rewrite Delete cache button in html and remove Babylon GUI

--- packages/core/src/point-cloud/utils/index.ts
+++ packages/core/src/point-cloud/utils/index.ts
@@ -11,14 +11,12 @@ import type { TileDBPointCloudOptions } from './tiledb-pc';
 import { setSceneColors, updateSceneColors } from './scene-colors';
 import { sortArrays, sortDataArrays } from './arrays';
 import { setCameraLight, setCameraPosition } from './camera-utils';
-import CacheGUI from './point-cloud-gui';
 import PointCloudGUI from './point-cloud-html-gui';
 import ParticleShaderMaterial from './particle-shader';
 
 export type { TileDBPointCloudOptions };
 
 export {
-  CacheGUI,
   getArrayMetadata,
   getArraySchema,
   getNonEmptyDomain,
