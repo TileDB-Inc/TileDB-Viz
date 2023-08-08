b4f9d9f5740d6fcd4598d7461652998c33607f95
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2023-08-08 17:33:30 +0300

    Added scalebar

--- packages/components/src/utils/helpers.ts
+++ packages/components/src/utils/helpers.ts
@@ -42,3 +42,59 @@ export function hexToRgb(hex: string): {r: number, g: number, b: number} | null 
     b: parseInt(result[3], 16)
   } : null;
 }
+
+const LENGTH_UNITS: Map<string, number> = new Map([
+  ['m', 0],
+  ['dm', -1],
+  ['cm', -2],
+  ['mm', -3],
+  ['μm', -6],
+  ['nm', -9],
+  ['pm', -12]
+]);
+
+const UNITS_ORDER = ['m', 'mm', 'μm', 'nm', 'pm'];
+
+export function lengthConverter(
+  value: number,
+  current_unit = 'μm'
+): [number, string] {
+  // The initial value units are always micrometers
+  if (value < 1) {
+    // If initial value is less than 1 we will try to express it in a unit whre it will be between [1, 1000)
+    let next_unit_index = UNITS_ORDER.indexOf(current_unit) + 1;
+
+    while (next_unit_index < UNITS_ORDER.length && value < 1) {
+      const next_unit = UNITS_ORDER[next_unit_index];
+
+      value *= Math.pow(
+        10,
+        LENGTH_UNITS.get(current_unit)! - LENGTH_UNITS.get(next_unit)!
+      );
+
+      current_unit = next_unit;
+      ++next_unit_index;
+    }
+  } else {
+    // Else if initial value is greater than 1000 we will try to express it in a unit whre it will be between [1, 1000)
+    let next_unit_index = UNITS_ORDER.indexOf(current_unit) - 1;
+
+    while (next_unit_index > -1 && value > 1000) {
+      const next_unit = UNITS_ORDER[next_unit_index];
+
+      value *= Math.pow(
+        10,
+        LENGTH_UNITS.get(current_unit)! - LENGTH_UNITS.get(next_unit)!
+      );
+
+      current_unit = next_unit;
+      --next_unit_index;
+    }
+  }
+
+  return [value, current_unit];
+}
+
+export function unitFormatter(value: number, unit: string): string {
+  return `${value.toFixed(2)} ${unit}`;
+}
