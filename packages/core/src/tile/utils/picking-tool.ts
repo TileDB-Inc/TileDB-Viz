4e3988af4eb656f720d6b794499687a774119590
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2023-10-27 12:00:54 +0300

    Added picking utility class

---
+++ packages/core/src/tile/utils/picking-tool.ts
@@ -0,0 +1,242 @@
+import {
+  Scene,
+  Observer,
+  Nullable,
+  KeyboardInfo,
+  KeyboardEventTypes,
+  PointerInfo,
+  PointerEventTypes,
+  UtilityLayerRenderer,
+  Mesh,
+  VertexData,
+  EventState,
+  VertexBuffer
+} from '@babylonjs/core';
+import { ScreenSpaceLineMaterial } from '../materials/screenSpaceLineMaterial';
+
+enum PickingMode {
+  NONE = 0,
+  SINGLE = 1,
+  LASSO = 3
+}
+
+export class PickingTool {
+  public pickCallbacks: {
+    (
+      bbox: number[],
+      constraints?: { path?: number[]; tiles?: number[][] }
+    ): void;
+  }[];
+
+  private mode: PickingMode;
+  private scene: Scene;
+  private utilityLayer: UtilityLayerRenderer;
+
+  private selectionBbox: number[];
+  private selectionBuffer: number[];
+
+  private lassoHandler: Nullable<Observer<PointerInfo>>;
+  private singleHandler: Nullable<Observer<PointerInfo>>;
+
+  private selectionRenderPositions: number[];
+  private selectionRenderIndices: number[];
+  private selectionRenderMesh: Mesh;
+  private selectionRenderVertexData: VertexData;
+  private selectedTiles: Set<string>;
+
+  private active: boolean;
+
+  constructor(scene: Scene) {
+    this.mode = PickingMode.NONE;
+    this.scene = scene;
+    this.utilityLayer = new UtilityLayerRenderer(this.scene, false);
+    this.selectionRenderVertexData = new VertexData();
+    this.selectionRenderMesh = new Mesh(
+      'selection',
+      this.utilityLayer.utilityLayerScene
+    );
+    this.selectionRenderMesh.alwaysSelectAsActiveMesh = true;
+    this.selectionRenderMesh.material = ScreenSpaceLineMaterial(
+      this.utilityLayer.utilityLayerScene
+    );
+    this.pickCallbacks = [];
+
+    this.singleHandler = null;
+    this.lassoHandler = null;
+
+    this.selectionBuffer = [];
+    this.selectionRenderPositions = [];
+    this.selectionRenderIndices = [];
+    this.selectionBbox = [];
+    this.selectedTiles = new Set<string>();
+    this.active = false;
+
+    this.scene.onKeyboardObservable.add((keyboardInfo: KeyboardInfo) => {
+      switch (keyboardInfo.type) {
+        case KeyboardEventTypes.KEYUP:
+          if (!keyboardInfo.event.shiftKey) {
+            break;
+          }
+
+          if (
+            keyboardInfo.event.code === 'Digit1' &&
+            this.mode !== PickingMode.SINGLE
+          ) {
+            this.mode = PickingMode.SINGLE;
+
+            this.scene.onPointerObservable.remove(this.lassoHandler);
+
+            this.singleHandler = this.scene.onPointerObservable.add(
+              this.singlePicker.bind(this),
+              undefined,
+              true
+            );
+          } else if (
+            keyboardInfo.event.code === 'Digit3' &&
+            this.mode !== PickingMode.LASSO
+          ) {
+            this.mode = PickingMode.LASSO;
+
+            this.scene.onPointerObservable.remove(this.singleHandler);
+
+            this.lassoHandler = this.scene.onPointerObservable.add(
+              this.lassoPicker.bind(this),
+              undefined,
+              true
+            );
+          } else if (keyboardInfo.event.code === 'Digit0') {
+            this.mode = PickingMode.NONE;
+
+            this.scene.onPointerObservable.remove(this.singleHandler);
+            this.scene.onPointerObservable.remove(this.lassoHandler);
+          }
+          break;
+      }
+    });
+  }
+
+  private singlePicker(pointerInfo: PointerInfo, state: EventState): void {
+    switch (pointerInfo.type) {
+      case PointerEventTypes.POINTERTAP: {
+        const [x, y] = [
+          pointerInfo.event.offsetX,
+          this.scene.getEngine().getRenderHeight() - pointerInfo.event.offsetY
+        ];
+
+        this.selectionBbox = [
+          Number.MAX_VALUE,
+          Number.MAX_VALUE,
+          -Number.MAX_VALUE,
+          -Number.MAX_VALUE
+        ];
+
+        this.selectionBbox[0] = Math.min(this.selectionBbox[0], Math.round(x));
+        this.selectionBbox[1] = Math.min(this.selectionBbox[1], Math.round(y));
+        this.selectionBbox[2] = Math.max(this.selectionBbox[2], Math.round(x));
+        this.selectionBbox[3] = Math.max(this.selectionBbox[3], Math.round(y));
+
+        const tile = pointerInfo.pickInfo?.pickedMesh?.name
+          .split(',')
+          .map(x => Number.parseInt(x));
+
+        this.pickCallbacks.forEach(callback =>
+          callback(this.selectionBbox, { tiles: tile ? [tile] : undefined })
+        );
+
+        break;
+      }
+    }
+  }
+
+  private lassoPicker(pointerInfo: PointerInfo, state: EventState): void {
+    state.skipNextObservers = true;
+
+    switch (pointerInfo.type) {
+      case PointerEventTypes.POINTERDOWN: {
+        this.active = true;
+        this.selectionBbox = [
+          Number.MAX_VALUE,
+          Number.MAX_VALUE,
+          -Number.MAX_VALUE,
+          -Number.MAX_VALUE
+        ];
+        this.selectionBuffer = [];
+        this.selectionRenderPositions = [];
+        this.selectionRenderIndices = [];
+        this.selectedTiles.clear();
+        this.selectionRenderMesh.removeVerticesData(VertexBuffer.PositionKind);
+        break;
+      }
+      case PointerEventTypes.POINTERUP: {
+        this.active = false;
+        this.pickCallbacks.forEach(callback =>
+          callback(this.selectionBbox, { path: this.selectionBuffer })
+        );
+        this.selectionRenderMesh.removeVerticesData(VertexBuffer.PositionKind);
+        break;
+      }
+      case PointerEventTypes.POINTERMOVE: {
+        if (!this.active) {
+          break;
+        }
+
+        const [x, y] = [
+          pointerInfo.event.offsetX,
+          this.scene.getEngine().getRenderHeight() - pointerInfo.event.offsetY
+        ];
+
+        const index = this.selectionBuffer.length;
+
+        if (
+          index === 0 ||
+          Math.abs(
+            (x - this.selectionBuffer[index - 2]) *
+              (y - this.selectionBuffer[index - 1])
+          ) > 6
+        ) {
+          this.selectionBuffer.push(x, y);
+
+          this.selectionBbox[0] = Math.min(
+            this.selectionBbox[0],
+            Math.round(x)
+          );
+          this.selectionBbox[1] = Math.min(
+            this.selectionBbox[1],
+            Math.round(y)
+          );
+          this.selectionBbox[2] = Math.max(
+            this.selectionBbox[2],
+            Math.round(x)
+          );
+          this.selectionBbox[3] = Math.max(
+            this.selectionBbox[3],
+            Math.round(y)
+          );
+
+          // Lasso rendering
+          this.selectionRenderIndices.push(
+            this.selectionRenderPositions.length / 3
+          );
+          this.selectionRenderPositions.push(x, y, 0);
+
+          this.selectionRenderVertexData.positions = Float32Array.from(
+            this.selectionRenderPositions
+          );
+          this.selectionRenderVertexData.indices = Int32Array.from(
+            this.selectionRenderIndices
+          );
+          this.selectionRenderVertexData.applyToMesh(
+            this.selectionRenderMesh,
+            false
+          );
+        }
+        break;
+      }
+    }
+  }
+
+  public dispose() {
+    this.scene.onPointerObservable.remove(this.singleHandler);
+    this.scene.onPointerObservable.remove(this.lassoHandler);
+  }
+}
