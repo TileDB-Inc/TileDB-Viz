f6cfb7326cdbbfec33624bcf4d50e96f3e1ff048
XanthosXanthopoulos <xanxanthopoulos@gmail.com>
2024-05-13 15:17:37 +0300

    Fix incompatible package versions

--- packages/react/src/stories/TileImage.stories.tsx
+++ packages/react/src/stories/TileImage.stories.tsx
@@ -25,33 +25,51 @@ if (process.env.STORYBOOK_NUM_WORKERS) {
 
 export const RasterPointCloud = () => (
   <TileImageVisualization
-    engineAPI={"WEBGPU"}
+    engineAPI={'WEBGPU'}
     token={token}
     namespace={'TileDB-Inc'}
     arrayID={'tiledb://TileDB-Inc/ee5eae5f-9f68-4471-a762-99e966cada1c'}
     pointGroupID={['tiledb://TileDB-Inc/a89e17ae-4fc7-433f-a2ee-856ee0ecf216']}
-    tileUris={['https://api.pdok.nl/kadaster/3d-basisvoorziening/ogc/v1_0/collections/terreinen/3dtiles']}
+    tileUris={[
+      'https://api.pdok.nl/kadaster/3d-basisvoorziening/ogc/v1_0/collections/terreinen/3dtiles'
+    ]}
     width={'100vw'}
     height={'100vh'}
-    defaultChannels={
-      [{index: 1, intensity: 2000}, {index: 2, intensity: 2000}, {index: 3, intensity: 2000}]
-    }
+    defaultChannels={[
+      { index: 1, intensity: 2000 },
+      { index: 2, intensity: 2000 },
+      { index: 3, intensity: 2000 }
+    ]}
     sceneConfig={{
-      pointConfigs: [{
-        pickable: false,
-        features: [
-          {
-            name: "Height",
-            type: FeatureType.RGB,
-            interleaved: true,
-            attributes: [
-              {name: 'Red', normalize: true, normalizationWindow: { min: 0, max: 255 }}, 
-              {name: 'Green', normalize: true, normalizationWindow: { min: 0, max: 255 }}, 
-              {name: 'Blue', normalize: true, normalizationWindow: { min: 0, max: 255 }}
-            ]
-          }
-        ]
-      }]
+      pointConfigs: [
+        {
+          pickable: false,
+          features: [
+            {
+              name: 'Height',
+              type: FeatureType.RGB,
+              interleaved: true,
+              attributes: [
+                {
+                  name: 'Red',
+                  normalize: true,
+                  normalizationWindow: { min: 0, max: 255 }
+                },
+                {
+                  name: 'Green',
+                  normalize: true,
+                  normalizationWindow: { min: 0, max: 255 }
+                },
+                {
+                  name: 'Blue',
+                  normalize: true,
+                  normalizationWindow: { min: 0, max: 255 }
+                }
+              ]
+            }
+          ]
+        }
+      ]
     }}
   />
 );
