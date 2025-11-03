4a0dac56f93d0107a104749e71834d0799a604a2
XanthosXanthopoulos <38084549+XanthosXanthopoulos@users.noreply.github.com>
2025-11-03 06:31:16 -0800

    Update release CI to use `npm` instead of `yarn` (#176)
    * Update release CI to use `npm` instead of `yarn`
    
    * Remove yarn references from publish script
    
    * Update subpackage dependency versions
    
    * Fix files directive in components subpackage
    
    * Force axios to disable `withCredentials`
    
    * Update TileDB Cloud depndency

--- packages/core/src/utils/getTileDBClient/getTileDBClient.ts
+++ packages/core/src/utils/getTileDBClient/getTileDBClient.ts
@@ -1,11 +1,16 @@
 import Client from '@tiledb-inc/tiledb-cloud';
+import axios from 'axios';
 
 const getTileDBClient = () => {
   let client: Client | undefined;
 
   return (config?: Client['config']) => {
     if (config) {
-      client = new Client(config);
+      client = new Client(config, axios.create({
+        ...axios.defaults,
+        // We are using a token to authenticate so we do not need to enable credentials
+        withCredentials: false
+      }));
     }
 
     // If there is a client already instatiated with an api key return it
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,16 +6,16 @@ export TMP_PACKAGE_JSON=/tmp/package_$PACKAGE_FOLDER.json
 echo Publishing @tiledb-inc/viz-$PACKAGE_FOLDER@$GIT_TAG_VERSION
 cd packages/$PACKAGE_FOLDER
 # Update package.json's version with the tag from the release
-jq ".version=\"$GIT_TAG_VERSION\"" package.json > $TMP_PACKAGE_JSON && mv $TMP_PACKAGE_JSON package.json
+jq --arg GIT_TAG_VERSION "$GIT_TAG_VERSION" '.version=$GIT_TAG_VERSION | if .dependencies."@tiledb-inc/viz-core"? then .dependencies."@tiledb-inc/viz-core"=$GIT_TAG_VERSION else . end | if .dependencies."@tiledb-inc/viz-components"? then .dependencies."@tiledb-inc/viz-components"=$GIT_TAG_VERSION else . end | if .dependencies."@tiledb-inc/viz-common"? then .dependencies."@tiledb-inc/viz-common"=$GIT_TAG_VERSION else . end' package.json > $TMP_PACKAGE_JSON && mv $TMP_PACKAGE_JSON package.json
 if [[ $GIT_TAG_VERSION == *"beta"* ]];
 then
 echo "Publishing beta version $GIT_TAG_VERSION";
-yarn npm publish --access public --tag beta
+npm publish --access public --tag beta
 elif [[ $GIT_TAG_VERSION == *"alpha"* ]];
 then
 echo "Publishing alpha version $GIT_TAG_VERSION";
-yarn npm publish --access public --tag alpha
+npm publish --access public --tag alpha
 else
 echo "Publishing new version $GIT_TAG_VERSION";
-yarn npm publish --access public
+npm publish --access public
 fi
